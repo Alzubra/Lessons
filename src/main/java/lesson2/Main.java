609dd773c0ba96145ea04553e236320d98851004
Alzubra <a.bakhvalov@ya.ru>
2017-11-18 20:12:03 +0300

    циклы for и массив

--- src/main/java/lesson2/Main.java
+++ src/main/java/lesson2/Main.java
@@ -1,10 +1,15 @@
 package lesson2;
 
-import lesson2.objects.A1;
-
 public class Main {
 
     public static void main(String[] args) {
-        A1 obj = new A1();
+        int[] mass = new int[4];
+        for(int i = 0; i < 4; i = i+1 ) {
+            mass[i] = i + 1;
+        }
+
+        for(int вещь : mass) {
+            System.out.println(вещь);
+        }
     }
 }
