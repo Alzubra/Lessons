b6e0f9a72c6b0196a15755cbf7d62087f4e650f3
Alzubra <a.bakhvalov@ya.ru>
2017-11-18 20:42:34 +0300

    циклы 2

--- src/main/java/lesson2/Main.java
+++ src/main/java/lesson2/Main.java
@@ -1,6 +1,14 @@
 package lesson2;
 
+import lesson2.objects.A1;
+
 public class Main {
+
+    public static A1 getNNN() {
+        String s = "ИМЯ";
+        return new A1();
+    }
+
 
     public static void main(String[] args) {
         int[] mass = new int[4];
@@ -11,5 +19,7 @@ public class Main {
         for(int вещь : mass) {
             System.out.println(вещь);
         }
+        A1 name = getNNN();
+        System.out.println(name);
     }
 }
